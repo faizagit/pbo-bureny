2dc0a1a855dd7556f475170bec41ef82094b757c
faizagit <faiz.agit2005@gmail.com>
2023-08-09 19:46:35 +0700

    nambah modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container my-5">
         <div class="login d-flex justify-content-end">
-            <a href="{{ route('index_login') }}" class="btn btn-secondary" style="width: 200px" >Login</a>
+            <a href="{{ route('index_login') }}" class="btn btn-secondary" style="width: 200px; background-color: #f3a024; border: none;" >Login</a>
         </div>
         <div class="title mb-5">
             <h1 class="text-center fw-bold">Ekstrakurikuler</h1>
@@ -18,7 +18,11 @@
                             <img src="{{ asset('assets/img/badminton.jpg') }}" class="rounded" style="max-width: 100%;" alt="">
                         </div>
                         <div class="desc">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quam saepe perspiciatis, magnam voluptas tempora error dolorum dolorem ipsum ullam cupiditate corrupti!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis odio quis architecto obcaecati, quam sed, rerum eveniet iusto natus, quod harum pariatur saepe <b>...</b></p>
+                        </div>
+                        <div class="detail d-flex mb-4" style="height: 20px">
+                            <a class="text-decoration-none detail-hover fs-5 "  href="" data-bs-toggle="modal" data-bs-target="#modal2">Detail
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -30,7 +34,11 @@
                             <img src="{{ asset('assets/img/balbalan.jpg') }}" class="rounded" style="max-width: 100%;" alt="">
                         </div>
                         <div class="desc">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quam saepe perspiciatis, magnam voluptas tempora error dolorum dolorem ipsum ullam cupiditate corrupti!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis odio quis architecto obcaecati, quam sed, rerum eveniet iusto natus, quod harum pariatur saepe <b>...</b></p>
+                        </div>
+                        <div class="detail d-flex mb-4" style="height: 20px">
+                            <a class="text-decoration-none detail-hover fs-5 "  href="" data-bs-toggle="modal" data-bs-target="#modal2">Detail
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -42,7 +50,11 @@
                             <img src="{{ asset('assets/img/basket.jpg') }}" class="rounded" style="max-width: 100%;" alt="">
                         </div>
                         <div class="desc">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quam saepe perspiciatis, magnam voluptas tempora error dolorum dolorem ipsum ullam cupiditate corrupti!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis odio quis architecto obcaecati, quam sed, rerum eveniet iusto natus, quod harum pariatur saepe <b>...</b></p>
+                        </div>
+                        <div class="detail d-flex mb-4" style="height: 20px">
+                            <a class="text-decoration-none detail-hover fs-5 "  href="" data-bs-toggle="modal" data-bs-target="#modal2">Detail
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -54,7 +66,11 @@
                             <img src="{{ asset('assets/img/taekwondo.jpg') }}" class="rounded" style="max-width: 100%;" alt="">
                         </div>
                         <div class="desc">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quam saepe perspiciatis, magnam voluptas tempora error dolorum dolorem ipsum ullam cupiditate corrupti!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis odio quis architecto obcaecati, quam sed, rerum eveniet iusto natus, quod harum pariatur saepe <b>...</b></p>
+                        </div>
+                        <div class="detail d-flex mb-4" style="height: 20px">
+                            <a class="text-decoration-none detail-hover fs-5 "  href="" data-bs-toggle="modal" data-bs-target="#modal2">Detail
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -66,7 +82,11 @@
                             <img src="{{ asset('assets/img/badminton.jpg') }}" class="rounded" style="max-width: 100%;" alt="">
                         </div>
                         <div class="desc">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quam saepe perspiciatis, magnam voluptas tempora error dolorum dolorem ipsum ullam cupiditate corrupti!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis odio quis architecto obcaecati, quam sed, rerum eveniet iusto natus, quod harum pariatur saepe <b>...</b></p>
+                        </div>
+                        <div class="detail d-flex mb-4" style="height: 20px">
+                            <a class="text-decoration-none detail-hover fs-5 "  href="" data-bs-toggle="modal" data-bs-target="#modal2">Detail
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -78,22 +98,34 @@
                             <img src="{{ asset('assets/img/badminton.jpg') }}" class="rounded" style="max-width: 100%;" alt="">
                         </div>
                         <div class="desc">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quam saepe perspiciatis, magnam voluptas tempora error dolorum dolorem ipsum ullam cupiditate corrupti!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis odio quis architecto obcaecati, quam sed, rerum eveniet iusto natus, quod harum pariatur saepe <b>...</b></p>
+                        </div>
+                        <div class="detail d-flex mb-4" style="height: 20px">
+                            <a class="text-decoration-none detail-hover fs-5 "  href="" data-bs-toggle="modal" data-bs-target="#modal2">Detail
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-4 mb-4">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="img pb-3">
-                            <img src="{{ asset('assets/img/badminton.jpg') }}" class="rounded" style="max-width: 100%;" alt="">
-                        </div>
-                        <div class="desc">
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui quam saepe perspiciatis, magnam voluptas tempora error dolorum dolorem ipsum ullam cupiditate corrupti!</p>
-                        </div>
+            
+        </div>
+        <div class="modal fade " id="modal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Futsal</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="image-modal">
+                        <img class="w-100 rounded" src="{{ asset('assets/img/balbalan.jpg') }}" alt="salah oi">
                     </div>
                 </div>
+                <div class="modal-footer">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque molestias aliquid quod voluptatum laboriosam dolorem error libero magnam perferendis corrupti. Architecto molestiae voluptates neque modi iure delectus placeat voluptatum dolor.
+                  <p></p>
+                </div>
+              </div>
             </div>
         </div>
     </div>
